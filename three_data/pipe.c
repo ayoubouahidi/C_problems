7caf65e2d817295c9d01ca9c9cf063244cc8f70c
ayoubouahidi <ouahidi.ayoub2002@gmail.com>
2025-09-10 20:00:06 +0100

    some exes

--- three_data/pipe.c
+++ three_data/pipe.c
@@ -263,41 +263,41 @@
 // 	}
 // }
 
-// int ft_popen(const char *file, char *const argv[], char type)
-// {
-// 	int pid;
-// 	int fd[2];
+int ft_popen(const char *file, char *const argv[], char type)
+{
+	int pid;
+	int fd[2];
 
-// 	if(!file || !argv || (type != 'r' && type != 'w'))
-// 		return -1;
-// 	// read : 0;
-// 	//  write : 1
+	if(!file || !argv || (type != 'r' && type != 'w'))
+		return -1;
+	// read : 0;
+	//  write : 1
 
-// 	if (pipe(fd) == -1)
-// 		return -1;
-// 	pid = fork();
-// 	if (pid == 0)
-// 	{
-// 		if (type == 'r')
-// 			dup2(fd[1], STDOUT_FILENO);
-// 		else
-// 			dup2(fd[0], STDIN_FILENO);
-// 		close(fd[0]);
-// 		close(fd[1]);
-// 		execvp(file, argv);
-// 		exit(1);
-// 	}
-// 	if(type == 'r')
-// 	{
-// 		close(fd[1]);
-// 		return fd[0];
-// 	}
-// 	else
-// 	{
-// 		close(fd[0]);
-// 		return fd[1];
-// 	}
-// }
+	if (pipe(fd) == -1)
+		return -1;
+	pid = fork();
+	if (pid == 0)
+	{
+		if (type == 'r')
+			dup2(fd[1], STDOUT_FILENO);
+		else
+			dup2(fd[0], STDIN_FILENO);
+		close(fd[0]);
+		close(fd[1]);
+		execvp(file, argv);
+		exit(1);
+	}
+	if(type == 'r')
+	{
+		close(fd[1]);
+		return fd[0];
+	}
+	else
+	{
+		close(fd[0]);
+		return fd[1];
+	}
+}
 
 // int main()
 // {
@@ -317,14 +317,19 @@
 int    picoshell(char **cmds[])
 {
 	int fd[2];
-	int i;
 	int pid;
+	int i;
+	int prev_fd;
+	int status;
+	int exit_code;
 
+	exit_code = -1;
+	prev_fd = -1;
 	i = 0;
 	while (cmds[i])
 	{
 		if (cmds[i + 1] && pipe(fd))
-			return 1;
+			return (1);
 		pid = fork();
 		if (pid == -1)
 		{
@@ -333,13 +338,42 @@ int    picoshell(char **cmds[])
 				close(fd[0]);
 				close(fd[1]);
 			}
-			return 1;
+			return(1);
 		}
 		if (pid == 0)
 		{
-			
+			if (prev_fd != -1)
+			{
+				if (dup2(prev_fd, STDIN_FILENO) != -1)
+					exit(1);
+				close(prev_fd);
+			}
+			if (cmds[i + 1])
+			{
+				close(fd[0]);
+				if (dup2(fd[1], STDOUT_FILENO) != -1)
+					exit(1);
+				close(fd[1]);
+			}
+			execvp(cmds[i][0], cmds[i]);
+			exit(1);
 		}
+		if (prev_fd != -1)
+			close(prev_fd);
+		if (cmds[i + 1])
+		{
+			close(fd[0]);
+			prev_fd = fd[1];
+		}
+		i++;
 	}
+	while (wait(&status) != -1)
+	{
+		if(WIFEXITED(status) && WEXITSTATUS(status) != 0)
+			exit_code = 1;
+	}
+	return(exit_code);
+	
 	
 }
 
